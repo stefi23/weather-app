34067970fd04c0388ef7c47733aaab4a2431286f
stefi23 <stefania.rosca2309@gmail.com>
2020-04-17 18:00:34 +0200

    endpoints created

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,9 @@ router.post("/address", (req, res) => {
 });
 
 // 3. Add an endpoint ​that receives an address, validate it and check the weather for the lat/lon of that address.
-router.post("/address/response", (req, res) => {
+router.post("/address/weather", async (req, res) => {
+  let location = { latitude: 0, longitude: 0 };
+
   const street = req.body.street.replace(/[ ,]+/g, "+");
   const streetNumber = req.body.streetNumber;
   const town = req.body.town;
@@ -43,36 +45,74 @@ router.post("/address/response", (req, res) => {
   const country = req.body.country;
   let finalAddress =
     street + "+" + streetNumber + "+" + town + "+" + postalCode + "+" + country;
-  axios
-    .get(
+  try {
+    let result = await axios.get(
       `https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=${mapsAPI}&searchtext=${encodeURIComponent(
         finalAddress
       )}`
-    )
-    .then((response) => {
-      let latitude =
-        response.data["Response"]["View"][0]["Result"][0]["Location"][
-          "DisplayPosition"
-        ]["Latitude"];
-      let longitude =
-        response.data["Response"]["View"][0]["Result"][0]["Location"][
-          "DisplayPosition"
-        ]["Longitude"];
-      console.log("latitude: ", latitude);
-      console.log("longitude: ", longitude);
-      let location = {
-        latitude: latitude,
-        longitude: longitude,
-      };
-      res.send(location);
-    })
-    .catch((error) => res.send(error));
+    );
+
+    let latitude =
+      result.data["Response"]["View"][0]["Result"][0]["Location"][
+        "DisplayPosition"
+      ]["Latitude"];
+    let longitude =
+      result.data["Response"]["View"][0]["Result"][0]["Location"][
+        "DisplayPosition"
+      ]["Longitude"];
+
+    location = {
+      latitude: latitude,
+      longitude: longitude,
+    };
+    // res.send("done");
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    let result = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${weatherAPI}&units=metric`
+    );
+    let weather = result.data["weather"][0]["main"];
+    let temp = result.data["main"]["temp"];
+    let weatherResult = {
+      temperature: temp,
+      weather: weather,
+    };
+    res.send(weatherResult);
+  } catch (error) {
+    console.error(error);
+  }
 });
+
+// router.post("/address/weather", (req, res) => {
+//   const street = req.body.street.replace(/[ ,]+/g, "+");
+//   const streetNumber = req.body.streetNumber;
+//   const town = req.body.town;
+//   const postalCode = req.body.postalCode;
+//   const country = req.body.country;
+//   let finalAddress =
+//     street + "+" + streetNumber + "+" + town + "+" + postalCode + "+" + country;
+//   try {
+//     let result = axios.get(
+//       `https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=${mapsAPI}&searchtext=${encodeURIComponent(
+//         finalAddress
+//       )}`
+//     );
+//     let latitude =
+//       result["Response"]["View"][0]["Result"][0]["Location"]["DisplayPosition"][
+//         "Latitude"
+//       ];
+//     console.log(latitude);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
 // Weather API
 router.get("/weather", (req, res) => {
   axios(
-    `http://api.openweathermap.org/data/2.5/forecast?q=Barcelona&appid=4214a458bdd687f791e52fd42fb12dd6`
+    `http://api.openweathermap.org/data/2.5/forecast?q=${location.latitude}&appid=${weatherAPI}`
   ).then((response) => res.send(response.data));
 });
 
