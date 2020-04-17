50d86756084e38758e56634d2fda88a469845a89
stefi23 <stefania.rosca2309@gmail.com>
2020-04-17 12:44:04 +0200

    Changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+router.get("/address", (req, res) => {
+  res.send("Hello address!");
+});
+router.get("/address/weather", (req, res) => {
+  res.send("Hello weather!");
 });
 
 module.exports = router;
